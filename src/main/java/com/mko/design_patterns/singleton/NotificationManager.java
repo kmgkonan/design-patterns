6a40fd18b4123a4108a67e9663d1a40286824155
Marc KONAN <marcgildas.konan@gmail.com>
2025-08-14 01:04:44 +0200

    Implementation Observer

--- src/main/java/com/mko/design_patterns/singleton/NotificationManager.java
+++ src/main/java/com/mko/design_patterns/singleton/NotificationManager.java
@@ -1,8 +1,15 @@
 package com.mko.design_patterns.singleton;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mko.design_patterns.domain.NotificationType;
+import com.mko.design_patterns.singleton.oberver.NotificationObserver;
+
 public class NotificationManager {
 
 	private static NotificationManager instance;
+	List<NotificationObserver> observers = new ArrayList<NotificationObserver>();
 	
 	private NotificationManager() {}
 	
@@ -11,5 +18,19 @@ public class NotificationManager {
 			instance = new NotificationManager();
 		}
 		return instance;
+	}
+	
+	public void addObserver(NotificationObserver observer) {
+		this.observers.add(observer);
+	}
+	
+	public void removeObserver(NotificationObserver observer) {
+		this.observers.remove(observer);
+	}
+	
+	public void notify(String message, NotificationType type) {
+		for(NotificationObserver observer : this.observers) {
+			observer.update(message, type);
+		}
 	}
 }
